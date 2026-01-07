b4ab4f2b0f6ea5cedda80b2b21903d5ba5f8c291
baihu <baihu3210@gmail.com>
2026-01-07 19:25:39 +0800

    chore: bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="baihu-logging",
-    version="0.1.2",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "requests",
